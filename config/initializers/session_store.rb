8df477faf5e008fd656f5c6838fc32905fd650bb
Joshua Kidd <kidd.josh.343@gmail.com>
2014-03-31 21:35:59 -0500

    Possibly fixed sessions issue leading to the CSRF issue possibly.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Blogee::Application.config.session_store :cookie_store, key: '_blogee_session'
+Blogee::Application.config.session_store :cookie_store, key: '_blogee_session', domain: :all
