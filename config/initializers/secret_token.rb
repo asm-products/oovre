702e32f111ff70be32450be862b08aa30dfdda7c
Joshua Kidd <kidd.josh.343@gmail.com>
2014-03-31 21:28:50 -0500

    Added profile link to image in header. Fixed CSRF issues... maybe.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -10,4 +10,4 @@
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
 secret = Rails.env.production? ? ENV['SECRET_TOKEN'] : 'e8e6fb08f22d83f91f9a41e010b0a28f7dc85a48dd899944124c07d9a13b2abf5b3de421b1f85066c840de9cfddc009c73896b39b0c1150db2921db2bfad1f25'
-Blogee::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+Blogee::Application.config.secret_key_base = secret
