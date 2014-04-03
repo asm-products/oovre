184781e9707615585745aa3ea83c7fdb563e1321
Josh Kidd <kidd.josh.343@gmail.com>
2014-04-03 16:55:19 -0500

    Debugging.... wtf is going on.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by_username(params[:username])
     Visit.track(@user, request.remote_ip)
+
+    puts @user.inspect
+    puts "\n"
+    puts current_user.inspect
   end
 
   def home
