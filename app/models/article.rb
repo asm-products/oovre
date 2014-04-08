83dc1e443da2607620af5af5a170f9b2593d0c12
Josh Kidd <kidd.josh.343@gmail.com>
2014-04-08 00:00:31 -0500

    More paperclip problems...

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,7 @@
 class Article < ActiveRecord::Base
-  attr_accessible :image_file_name, :image_file_size, :image_content_type, :image
-
   default_scope { order("created_at DESC") }
 
   has_attached_file :image, :styles => { :large => "900x900>", :small => "300x300#" }
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
   belongs_to :article_set
