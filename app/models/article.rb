374d1e668be2ef7db4ed6c7ded0145b3ca538f80
Josh Kidd <kidd.josh.343@gmail.com>
2014-04-08 00:19:47 -0500

    FIXED IT ALL! :) Well.... just small paperclip things...

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,7 @@ class Article < ActiveRecord::Base
   default_scope { order("created_at DESC") }
 
   has_attached_file :image, :styles => { :large => "900x900>", :small => "300x300#" }
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
   belongs_to :user
   belongs_to :article_set
