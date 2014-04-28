d96b55ebc7d1d491861478334bc9d1bba1fb7be2
Josh Kidd <kidd.josh.343@gmail.com>
2014-04-28 09:33:07 -0500

    Switched unique views and total views around in dashboard... had them backwards, oops.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,8 +3,8 @@ class DashboardController < ApplicationController
   def index
     @articles = current_user.articles.recent
     @articles_today = current_user.articles.today.recent
-    @total_views_today = current_user.article_views_unique_today
-    @unique_views_today = current_user.article_views_total_today
+    @total_views_today = current_user.article_views_total_today
+    @unique_views_today = current_user.article_views_unique_today
   end
 
 end
