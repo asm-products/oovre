6243e613437e438743e89978557bb441e311bf78
Josh Kidd <kidd.josh.343@gmail.com>
2014-04-29 08:38:59 -0500

    did something... forgot what...

--- app/models/visit_detail.rb
+++ app/models/visit_detail.rb
@@ -1,3 +1,6 @@
 class VisitDetail < ActiveRecord::Base
+
+  scope :today, -> { where("visit_details.created_at >= ? and visit_details.created_at <= ?", Date.today.beginning_of_day, Date.today.end_of_day) }
+
   belongs_to :visit
 end
