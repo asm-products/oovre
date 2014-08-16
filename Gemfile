c6169685485c11b2dcb930cdc723f6efeea32e03
Joshua Kidd <kidd.josh.343@gmail.com>
2014-08-15 21:18:37 -0700

    What was changed?

--- Gemfile
+++ Gemfile
@@ -5,7 +5,6 @@ ruby '2.0.0'
 gem 'foreman'
 gem 'thin'
 gem 'rails', '4.1.0'
-gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
 gem 'jquery-rails'
 
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -4,16 +4,16 @@ class Visit < ActiveRecord::Base
     has_many :visit_details
 
     def self.track(obj, request)
-        visit = Visit.find_or_create_by(visitable_id: obj.id, visitable_type: obj.class.name)
-        unless VisitDetail.find_by_visit_id_and_ip_address(visit.id, request.remote_ip)
-          visit.increment!(:unique_visits)
-        end
-        visit.increment!(:total_visits)
-        visit_detail = visit.visit_details.create(
-            :ip_address => request.remote_ip, 
-            :referrer => request.env["HTTP_REFERER"], 
-            :user_agent => request.env["HTTP_USER_AGENT"]
-        )
+      visit = Visit.find_or_create_by(visitable_id: obj.id, visitable_type: obj.class.name)
+      unless VisitDetail.find_by_visit_id_and_ip_address(visit.id, request.remote_ip)
+        visit.increment!(:unique_visits)
+      end
+      visit.increment!(:total_visits)
+      visit_detail = visit.visit_details.create(
+        :ip_address => request.remote_ip, 
+        :referrer => request.env["HTTP_REFERER"], 
+        :user_agent => request.env["HTTP_USER_AGENT"]
+      )
     end
 
 end
